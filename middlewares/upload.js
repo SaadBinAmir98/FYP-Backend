e6a2db56de0bd33ec99795def5c3891e006c954c
owner <owner@owners-MacBook-Air.local>
2024-06-09 22:56:01 +0500

    uploads file added and multer updated PR

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,11 +1,19 @@
 const multer = require('multer');
+const fs = require('fs');
 const path = require('path');
+
+
+
+const uploadDir = path.join(__dirname, '..', 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Set up storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
 // Save files to the 'uploads' directory
-    cb(null, 'uploads/') 
+    cb(null, uploadDir) 
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
